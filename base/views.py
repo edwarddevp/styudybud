8a8fd619b30c1657ed9956b822be8a8002a9c47e
Edward Bocaranda <edwarddevp@gmail.com>
2025-04-02 16:48:08 -0400

    Add models for Room, Topic, and Message with corresponding migrations and update admin registration

--- base/views.py
+++ base/views.py
@@ -1,20 +1,13 @@
-from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': 'Lets learn Python!'},
-    {'id': 2, 'name': 'Design with me!'},
-    {'id': 3, 'name': 'Frontend developers!'},
-]
+from django.shortcuts import render, get_object_or_404
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
+    # room = Room.objects.get(pk=pk)
+    room = get_object_or_404(Room, pk=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
