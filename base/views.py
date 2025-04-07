b1ca12dd738b60e1d50938c9c13cf0ec8e669a30
Edward Bocaranda <edwarddevp@gmail.com>
2025-04-07 17:53:05 -0400

    Add participants feature to rooms and implement message deletion

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q, Count
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 def loginPage(request):
@@ -75,7 +75,19 @@ def home(request):
 def room(request, pk):
     # room = Room.objects.get(pk=pk)
     room = get_object_or_404(Room, pk=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created') 
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -120,4 +132,18 @@ def deleteRoom(request, pk):
         return redirect('home')
     
     context = {'title': room.name}
+    return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    
+    context = {'title': 'Message'}
     return render(request, 'base/delete.html', context)
